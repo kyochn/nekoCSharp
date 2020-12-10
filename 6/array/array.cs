0cd06e3f3bc3666e787c7ec65e2f337589e7fd8a
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-12-11 01:19:32 +0900

    array02

---
+++ 6/array/array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static void Main(){
+        int[,] myArray={{1,2,3},{4,5,6}};
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                Console.WriteLine("myArray[{0},{1}]={2}",i,j,myArray[i,j]);
+            }
+        }
+    }
+}
