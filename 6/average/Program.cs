0a16418d4c8d747097c42ae1c1cf2e25c0be5e80
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-12-11 00:53:49 +0900

    init

---
+++ 6/average/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Average02
+{
+    public static void Main()
+    {
+        int[] point={70,80,50};
+        int sum = 0,no;
+
+        no=point.Length;
+
+        for(int i=0;i<no;i++) sum+=point[i];
+
+        double average=(double)sum/no;
+
+        Console.WriteLine("合計={0},平均={1:##.#}",sum,average);
+    }
+}
