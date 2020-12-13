083cf91513e370d4fd25e78b9eeb7f81a5c5eb7e
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-12-14 02:35:29 +0900

    var03

---
+++ 6/var/var.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Var {
+    public static void Main () {
+        var name = new []{ "太郎", "次郎", "三郎", "四郎" };
+
+        for (var i = 0; i < name.Length; i++) {
+            Console.WriteLine (name[i]);
+        }
+
+        var f=new[]{0.5,0.9,1.5,2.3};
+        for(var i=0;i<name.Length;i++){
+            Console.WriteLine(f[i]);
+        }
+    }
+}
