7dfef4bc4758a6d20cdc02d822e42e149e96bd08
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-12-14 03:25:53 +0900

    sort01

---
+++ 6/sort/sort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Sort
+{
+    public static void Main()
+    {
+        string[] name=new string[5]{
+            "お","う","え","あ","い"
+        };
+
+        for(int i=0;i<name.Length;i++){
+            Console.WriteLine(name[i]);
+        }
+        Console.WriteLine();
+
+        Array.Sort(name);
+        for(int i=0;i<name.Length;i++){
+            Console.WriteLine(name[i]);
+        }
+        Console.WriteLine();
+
+        Array.Reverse(name);
+        for(int i=0;i<name.Length;i++){
+            Console.WriteLine(name[i]);
+        }
+    }
+}
