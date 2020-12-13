f6050bf4ce0b9ad244187cade4d6f43fed697107
kyochn <is0586vi@ed.ritsumei.ac.jp>
2020-12-14 06:03:33 +0900

    practice6_1

---
+++ 6/practice6-1/practice6-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class practice6_1
+{
+    public static void Main()
+    {
+        int usernum=int.Parse(Console.ReadLine());
+        double[] point = new double[usernum];
+        
+        for(int i=0;i<point.Length;i++)
+        {
+            point[i]=int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine();
+        
+        double ave=0.0;
+        foreach(float i in point) ave+=i;
+        Console.WriteLine(ave/usernum);
+        Console.WriteLine();
+
+        Array.Sort(point);
+        Array.Reverse(point);
+        
+        foreach(int i in point)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
