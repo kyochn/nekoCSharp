a0730b8d2e350bbd414e709654ad4f8116587fdc
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-10 00:41:04 +0900

    feat: オーバーロードを利用してint,daouble型の値の和を求める

---
+++ 7/practice7-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Total
+{
+    public Total(int i1,int i2)
+    {
+        Console.WriteLine(i1+i2);
+    }
+    public Total(double d1,double d2)
+    {
+        Console.WriteLine(d1+d2);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int a=10,b=20;
+        double c=1.7,d=2.5;
+        Total iTo = new Total(a,b);
+        Total dTo = new Total(c,d);
+    }
+}
+
