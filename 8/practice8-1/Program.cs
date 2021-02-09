f7ed49ad1b216e1706e4e8ca40dd2a6e9a5c85fd
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-10 05:23:22 +0900

    feat: プロパティで正の数しか入力できないようにした

---
+++ 8/practice8-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Myclass
+{
+    double b1;
+    public double MyPro
+    {
+        get
+        {
+            return b1;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("入力できるのは正の数のみです");
+                return;
+            }
+            b1 = value;
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("正の整数を入力：");
+        Myclass mc = new Myclass();
+        do
+        {
+            mc.MyPro = double.Parse(Console.ReadLine());
+        } while (mc.MyPro <= 0);
+        Console.WriteLine("入力した数：{0}", mc.MyPro);
+    }
+}
