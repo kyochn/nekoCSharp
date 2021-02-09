773c8be7c96b4ef22275e92032913618d5adf6ac
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-10 00:13:59 +0900

    feat: 別クラスのインスタンス変数をMainメソッドから読み出す

---
+++ 7/practice7-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class IntClass
+{
+    public int x;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        IntClass num = new IntClass();
+        num.x=int.Parse(Console.ReadLine());
+        Console.WriteLine(num.x);
+    }
+}
